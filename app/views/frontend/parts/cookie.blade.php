c6b2c161e91af34c4c7b2f4d55ea69a5ac052f42
Mateusz Pęczkowski <peczis@gmail.com>
2016-11-28 21:03:52 +0100

    cookie fix

--- app/views/frontend/parts/cookie.blade.php
+++ app/views/frontend/parts/cookie.blade.php
@@ -1,5 +1,5 @@
 <div id="cookieWarning">
 	<strong>{{ trans('site.cookies.title') }}</strong>
 	<p>{{ trans('site.cookies.content') }}</p>
-	<span class="cookie-btn | js-cookie-close">{{ trans('site.cookies.button') }}</span>
+	<span class="cookie-btn | js-cookie-close"><i class="icon icon-cross"></i></span>
 </div>
