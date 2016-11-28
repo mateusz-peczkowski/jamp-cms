eb5f9d194819f0314e731ae82915b5ddfd5ff9fd
Mateusz Pęczkowski <peczis@gmail.com>
2016-11-28 16:38:10 +0100

    socials

---
+++ app/config/settings.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+
+    'base'   => array(
+        '0' => 'star'
+    ),
+
+    'socials'	=> array(
+        'socialFacebook' => 'facebook',
+        'socialInstagram' => 'instagram',
+        'socialTripadvisor' => 'tripadvisor',
+        'socialYoutube' => 'youtube',
+        'socialTwitter' => 'twitter',
+        'socialGoogle' => 'google-plus',
+        'socialPinterest' => 'pinterest',
+        'socialVimeo' => 'vimeo',
+	)
+
+);
--- app/helpers.php
+++ app/helpers.php
@@ -1,21 +1,35 @@
 <?php 
-	$obj = (object) [
-		'id' => 'default',
-		'title' => 'default'
-	];
-	$articlespaths = array($obj);
-	if(File::files(app_path().'/views/frontend/articles')) {
-		$files = File::files(app_path().'/views/frontend/articles');
-		foreach($files as $tmppath) {
-			$pathname = str_replace('.blade', '', pathinfo($tmppath)['filename']);
-			$obj = (object) [
-				'id' => $pathname,
-				'title' => $pathname
-			];
-			if(!in_array($obj, $articlespaths)) {
-				array_push($articlespaths, $obj);
+	function articlePaths() 
+	{
+		$obj = (object) [
+			'id' => 'default',
+			'title' => 'default'
+		];
+		$articlespaths = array($obj);
+		if(File::files(app_path().'/views/frontend/articles')) {
+			$files = File::files(app_path().'/views/frontend/articles');
+			foreach($files as $tmppath) {
+				$pathname = str_replace('.blade', '', pathinfo($tmppath)['filename']);
+				$obj = (object) [
+					'id' => $pathname,
+					'title' => $pathname
+				];
+				if(!in_array($obj, $articlespaths)) {
+					array_push($articlespaths, $obj);
+				}
 			}
 		}
+		View::share('articlespaths', $articlespaths);
 	}
-	View::share('articlespaths', $articlespaths);
-?>
+	articlePaths();
+
+	function iconIdToName($id, $arr = 'base')
+	{
+	    $iconArray = Config::get('settings.'.$arr);
+
+	    if(isset($iconArray[$id])) {
+	        return $iconArray[$id];
+	    }
+
+	    return 'star';
+	}
