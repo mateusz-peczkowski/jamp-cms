ec7055de742b6285f694d9648cb039ccf6b077c5
Mateusz Pęczkowski <peczis@gmail.com>
2016-11-28 03:48:13 +0100

    fix

--- app/views/frontend/parts/articles.blade.php
+++ app/views/frontend/parts/articles.blade.php
@@ -1,6 +1,24 @@
-<?php $articles = $global_page->connection('Article') ? : false; ?>
+<?php 
+	$articles = $global_page->connection('Article') ? : false; 
+	$checkNums = (object) [];
+	foreach($articlespaths as $path) {
+		$title = $path->title;
+		$checkNums->$title = 1;
+	}
+?>
 @if($articles AND sizeof($articles))
 	@foreach($articles as $article)
-		@include('frontend.articles.'.($article->viewinc ? : 'default'))
+		<?php 
+			$view = ($article->viewinc ? : 'default');
+			$checknum = $checkNums->$view;
+		?>
+		@include('frontend.articles.'.$view)
+		<?php 
+			if ($checkNums->$view) {
+				$checkNums->$view = $checkNums->$view+1;
+			} else {
+				$checkNums->$view = 1;
+			}
+		?>
 	@endforeach
 @endif
