8baf5527a9beb49ad67179ee8087521bad3d9eca
Mateusz Pęczkowski <peczis@gmail.com>
2016-11-29 21:26:00 +0100

    head to parts

--- app/views/frontend/layouts/base.blade.php
+++ app/views/frontend/layouts/base.blade.php
@@ -1,59 +1,26 @@
 <!DOCTYPE html>
 <html lang="pl">
-  <head>
-    <meta charset="utf-8">
-    @if($global_page->meta('title'))
-        <title>{{ $global_page->meta('title') }}</title>
-    @elseif($defaultpage->meta('title'))
-        <title>{{ $defaultpage->meta('title') }}</title>
-    @endif
-    @if($global_page->meta('description'))
-        <meta name="description" content="{{ $global_page->meta('description') }}">
-    @elseif($defaultpage->meta('robots'))
-        <meta name="description" content="{{ $defaultpage->meta('description') }}">
-    @endif
-    @if($global_page->meta('keywords'))
-        <meta name="keywords" content="{{ $global_page->meta('keywords') }}">
-    @elseif($defaultpage->meta('robots'))
-        <meta name="keywords" content="{{ $defaultpage->meta('keywords') }}">
-    @endif
-    @if($global_page->meta('robots'))
-        <meta name="robots" content="{{ $global_page->meta('robots') }}">
-    @elseif($defaultpage->meta('robots'))
-        <meta name="robots" content="{{ $defaultpage->meta('robots') }}">
-    @endif
-    <meta name="author" content="JAMPstudio.pl">
+    <head>
+        @include('frontend.parts.head')
+    </head>
+    <body class="{{ isset($isHome) ? 'hp' : 'sec' }}">
+        @include('frontend.parts.cookie')
 
-    <!-- Mobile Specific Metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        
-    <!--Favicons -->
-    @include('frontend/parts/favicons') 
+        @include('frontend.parts.header')
 
-     <!--styles -->
-     <link rel="stylesheet" type="text/css" href="/css/app.css">
+        <div id="wrapper">
 
-    {{ $global_page->meta('head') }}
-</head>
-<body class="{{ isset($isHome) ? 'hp' : 'sec' }}">
-@include('frontend.parts.cookie')
+            @yield('main')
+            
+        </div>
 
-@include('frontend.parts.header')
+        @include('frontend.parts.footer')
 
-<div id="wrapper">
+        @section('scripts')
+            <script src="/js/app.js"></script>
+            <script src="/js/trans/{{\Config::get('app.locale')}}.js"></script>
+        @show
 
-    @yield('main')
-    
-</div>
-
-@include('frontend.parts.footer')
-
-@section('scripts')
-    <script src="/js/app.js"></script>
-    <script src="/js/trans/{{\Config::get('app.locale')}}.js"></script>
-@show
-
-
-{{ $global_page->meta('footer') }}
-</body>
+        {{ $global_page->meta('footer') }}
+    </body>
 </html>
---
+++ app/views/frontend/parts/head.blade.php
@@ -0,0 +1,33 @@
+<meta charset="utf-8">
+@if($global_page->meta('title'))
+    <title>{{ $global_page->meta('title') }}</title>
+@elseif($defaultpage->meta('title'))
+    <title>{{ $defaultpage->meta('title') }}</title>
+@endif
+@if($global_page->meta('description'))
+    <meta name="description" content="{{ $global_page->meta('description') }}">
+@elseif($defaultpage->meta('robots'))
+    <meta name="description" content="{{ $defaultpage->meta('description') }}">
+@endif
+@if($global_page->meta('keywords'))
+    <meta name="keywords" content="{{ $global_page->meta('keywords') }}">
+@elseif($defaultpage->meta('robots'))
+    <meta name="keywords" content="{{ $defaultpage->meta('keywords') }}">
+@endif
+@if($global_page->meta('robots'))
+    <meta name="robots" content="{{ $global_page->meta('robots') }}">
+@elseif($defaultpage->meta('robots'))
+    <meta name="robots" content="{{ $defaultpage->meta('robots') }}">
+@endif
+<meta name="author" content="JAMPstudio.pl">
+
+<!-- Mobile Specific Metas -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    
+<!--Favicons -->
+@include('frontend/parts/favicons') 
+
+ <!--styles -->
+ <link rel="stylesheet" type="text/css" href="/css/app.css">
+
+{{ $global_page->meta('head') }}
